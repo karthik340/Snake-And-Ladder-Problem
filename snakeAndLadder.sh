f166351002519e63184571beebccbbb13e0c90f2
karthik <nallaboluk@gmail.com>
2020-06-11 09:28:40 +0530

    adding playing game with 2 players

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -34,18 +34,60 @@ function checkForOptions {
 	return $position
 }
 
-countNoOfDies=0
-currentPosition=$startPosition
-while [	$currentPosition -ne 100 ] 
-do
-	dieValue=$(rollDie)
-	countNoOfDies=$(($countNoOfDies+1))
+
+function getNextMove {
+	local dieValue=$(rollDie)
 	echo 'dieValue = '$dieValue
+	local currentPosition=$1
+	echo "currentPosition = "$currentPosition
 	checkForOptions $currentPosition $dieValue
-	dest=$?
-	currentPosition=$dest
-	echo 'Position after die role = '$currentPosition
+	local nextPosition=$?
+	echo "nextPosition = "$nextPosition
+	return $nextPosition
+}
+
+
+player1Position=$startPosition
+player2Position=$startPosition
+chance=1
+countNoOfDies1=0
+countNoOfDies2=0
+while [ $player1Position -ne 100 -a $player2Position -ne 100 ]
+do
+	if [ $chance -eq 1 ]
+	then
+		echo 
+		echo "player-1"
+		getNextMove $player1Position
+		player1Position=$?
+		chance=2
+		countNoOfDies1=$(($countNoOfDies1+1))
+	else	
+		echo
+		echo "player-2"
+		getNextMove $player2Position
+		player2Position=$?
+		chance=1
+		countNoOfDies2=$(($countNoOfDies2+1))
+	fi
 done
 
-echo "Number of times dies played to win game ="$countNoOfDies 
+echo
+echo "player1 = "$player1Position  
+echo "player2 = "$player2Position  
+
+echo 
+if [ $player1Position -eq 100 ]
+then
+	echo "player-1 won match"
+	echo "number of dies played by player-1 to win match = "$countNoOfDies1
+else
+	echo "player-2 won match"
+	echo "number of dies played by player-2 to win match = "$countNoOfDies2	
+fi
+
+
+
+
+
 
