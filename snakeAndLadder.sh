c01d4f238938b6680a122e5bda51b70915dd3f45
karthik <nallaboluk@gmail.com>
2020-06-11 08:45:58 +0530

    adding checking option and make move

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -7,5 +7,28 @@ function rollDie {
 
 }
 
-result=$(rollDie)
-echo $result
+function checkForOptions {
+	local position=$1
+	local dieValue=$2
+	local options=("NoPlay" "Ladder" "Snake")
+	local index=$((RANDOM%3))
+	case ${options[$index]} in
+		NoPlay) 
+			;;
+		Ladder)
+				position=$(($position+$dieValue))
+			;;
+		Snake)
+				position=$(($position-$dieValue))
+			;;
+	esac
+	return $position
+}
+
+
+dieValue=$(rollDie)
+checkForOptions $startPosition $dieValue
+dest=$?
+
+echo $dest
+
